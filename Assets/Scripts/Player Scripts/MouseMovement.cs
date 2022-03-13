88219d9c81fdaeaba8c58e46c2aff0c18458cf32
TheFishFlopper <alex@golf18.ca>
2022-03-13 00:04:46 -0500

    Fixed Controls
    Controls were bad, and now they're good, and they're moving into your neighbourhood.

--- Assets/Scripts/Player Scripts/MouseMovement.cs
+++ Assets/Scripts/Player Scripts/MouseMovement.cs
@@ -28,21 +28,9 @@ namespace McgillTeam3
         void Update()
         {
             Vector2 direction = _targetPos - (Vector2) transform.position; // Direction towards the mouse.
-
-            if (direction.magnitude >= idle_distance){ // Mouse outside idle
-                // Increase move speed.
-                move_speed += acceleration;
-                if (move_speed > max_move_speed.Value) move_speed = max_move_speed.Value; // Clamp to max speed
-            } 
-
-            else { // Mouse inside idle
-                // Decrease move speed.
-                move_speed -= deceleration;
-                if (move_speed < 0f) move_speed = 0f; // Clamp to 0 speed.
-            }
-
+            
             // Update velocity towards mouse.
-            rigidBody.velocity = direction.normalized * move_speed;
+            rigidBody.velocity = 2f * direction;
         }
     }
 }
