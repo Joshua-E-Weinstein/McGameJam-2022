9da0488e11af81bb79e322327922ec954fbce56e
TheFishFlopper <alex@golf18.ca>
2022-03-13 03:42:44 -0400

    Game Only Starts When You Hit Space

---
+++ Assets/Scripts/Player Scripts/PauseEvent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace McgillTeam3
+{
+    public class PauseEvent : MonoBehaviour
+    {
+        [SerializeField] private GameObject player;
+
+        private Echolocation _echolocation;
+
+         private void Awake()
+        {
+            _echolocation = player.GetComponent<Echolocation>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Time.timeScale = 0;
+        }
+
+        private void OnEnable()
+        {
+            Echolocation.OnStartEcholocate += OnStartEcholocate;
+        }
+
+        private void OnDisable()
+        {
+            Echolocation.OnStartEcholocate -= OnStartEcholocate;
+        }
+
+        private void OnStartEcholocate()
+        {
+            Time.timeScale = 1;
+            gameObject.SetActive(false);
+        }
+    }
+}
